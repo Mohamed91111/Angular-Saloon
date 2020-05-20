acf4f151adf49f9027e702ca2bdd60c4915a6269
mohamed albushra <mohamed.albushra@outlook.com>
2020-05-20 17:04:49 +0200

    starting the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DialogueComponent } from './dialogue/dialogue.component';
+import { SelectBeverageComponent } from './dialogue/select-beverage/select-beverage.component';
+import { TheUsualComponent } from './dialogue/the-usual/the-usual.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogueComponent,
+    SelectBeverageComponent,
+    TheUsualComponent
   ],
   imports: [
     BrowserModule,
